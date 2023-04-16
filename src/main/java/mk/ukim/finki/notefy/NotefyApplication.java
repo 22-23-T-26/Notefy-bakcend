83a8d5654cc1cdc7232f3e872e9e30621d20dd14
Jeton Ramadani <jeton.ramadani@students.finki.ukim.mk>
2023-04-16 04:22:49 +0200

    Disabled spring security until config, created dev profile with connection to a local h2 db file and updated gitignore to ignore the folder of the local dbs on commit

--- src/main/java/mk/ukim/finki/notefy/NotefyApplication.java
+++ src/main/java/mk/ukim/finki/notefy/NotefyApplication.java
@@ -2,8 +2,13 @@ package mk.ukim.finki.notefy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+/**
+ * TODO exclude = SecurityAutoConfiguration.class remove after config of security
+ */
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NotefyApplication {
 
 	public static void main(String[] args) {
