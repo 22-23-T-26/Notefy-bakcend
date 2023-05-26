25f95bff72ad2aa0125fa33ab24f974fa1244fc6
DraganaTanevska191079 <dragana.tanevska@students.finki.ukim.mk>
2023-05-26 15:48:41 +0200

    Added controller for posts

--- src/main/java/mk/ukim/finki/notefy/model/entities/Post.java
+++ src/main/java/mk/ukim/finki/notefy/model/entities/Post.java
@@ -29,7 +29,6 @@ public class Post {
     private String title;
     private String description;
     public long price;
-    private File file;
     private String url;
     private File picture;
     @ManyToOne
