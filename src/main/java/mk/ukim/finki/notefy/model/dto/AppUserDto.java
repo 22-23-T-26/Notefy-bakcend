2b0866cbc2cf5d2a14a98ce49371ef1285572717
Aleksandar Budjakoski <94196244+aleksandarbudzakoski@users.noreply.github.com>
2023-05-26 16:24:48 +0200

    Feature/user controller (#7)
    * feature/userController: add method for returning the users rating.
    
    * feature/userController: implement userContrller
    
    * feature/userController: remove password for profile details.
    
    ---------
    
    Co-authored-by: Aleksandar Budjakoski <Aleksandar.Budjakoski@netcetera.com>

---
+++ src/main/java/mk/ukim/finki/notefy/model/dto/AppUserDto.java
@@ -0,0 +1,32 @@
+package mk.ukim.finki.notefy.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mk.ukim.finki.notefy.model.entities.Post;
+
+import java.util.List;
+
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUserDto {
+
+  private String firstName;
+
+  private String lastName;
+
+  private String username;
+
+  private double rating;
+
+  private String email;
+
+  private String phoneNumber;
+
+  private List<Post> postsOfUsers;
+
+}
--- src/main/java/mk/ukim/finki/notefy/service/UserService.java
+++ src/main/java/mk/ukim/finki/notefy/service/UserService.java
@@ -9,13 +9,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
 
 //    public AppUser findByUsernameAndPassword(String username, String password) {
@@ -23,43 +27,63 @@ public class UserService {
 //                .orElseThrow(() -> new BadRequest("User with those credentials doesn't exist"));
 //    }
 
-    public AppUser getByUsernameOrEmailOrNull(String username) {
-        return userRepository.getByUsernameOrEmail(username, username)
-                .orElse(null);
+  public AppUser getByUsernameOrEmailOrNull(String username) {
+    return userRepository.getByUsernameOrEmail(username, username)
+        .orElse(null);
+  }
+
+
+  public AppUser getByUsernameOrNull(String username) {
+    return userRepository.findByUsername(username).orElse(null);
+  }
+
+  public AppUser createUser(RegisterDto registerDto) {
+    AppUser user = getByUsernameOrNull(registerDto.getUsername());
+    if (user != null) {
+      throw new BadRequest("User with username: " + registerDto.getUsername() + " already exists!");
+    }
+    user = userRepository.findByEmail(registerDto.getEmail()).orElse(null);
+    if (user != null) {
+      throw new BadRequest("User with email: " + registerDto.getEmail() + " already exists!");
     }
 
+    return userRepository.save(
+        AppUser.builder()
+            .username(registerDto.getUsername())
+            .password(passwordEncoder.encode(registerDto.getPassword()))
+            .email(registerDto.getEmail())
+            .firstName(registerDto.getFirstName())
+            .lastName(registerDto.getLastName())
+            .phoneNumber(registerDto.getPhoneNumber())
+            .role(registerDto.getRole())
+            .build()
+    );
+  }
 
-    public AppUser getByUsernameOrNull(String username) {
-        return userRepository.findByUsername(username).orElse(null);
+  public AppUser getCurrentUser() {
+    String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+    return userRepository.getByUsernameOrEmail(username, username)
+        .orElseThrow(() -> new BadRequest("You are not authenticated"));
+  }
+
+  public double getRatingForUser(String username) {
+
+    var user = userRepository.findByUsername(username);
+    if (user.isEmpty()) {
+      return 0.0;
     }
-    public AppUser createUser(RegisterDto registerDto) {
-        AppUser user = getByUsernameOrNull(registerDto.getUsername());
-        if (user != null) {
-            throw new BadRequest("User with username: " +registerDto.getUsername() + " already exists!");
-        }
-        user = userRepository.findByEmail(registerDto.getEmail()).orElse(null);
-        if (user != null) {
-            throw new BadRequest("User with email: " +registerDto.getEmail() + " already exists!");
-        }
+    final var existingUser = user.get();
 
-        return userRepository.save(
-                AppUser.builder()
-                        .username(registerDto.getUsername())
-                        .password(passwordEncoder.encode(registerDto.getPassword()))
-                        .email(registerDto.getEmail())
-                        .firstName(registerDto.getFirstName())
-                        .lastName(registerDto.getLastName())
-                        .phoneNumber(registerDto.getPhoneNumber())
-                        .role(registerDto.getRole())
-                        .build()
-        );
-    }
+    List<Integer> ratings = new ArrayList<>();
 
-    public AppUser getCurrentUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        return userRepository.getByUsernameOrEmail(username, username)
-                .orElseThrow(() -> new BadRequest("You are not authenticated"));
-    }
+    existingUser.getPostsOfUsers().stream()
+        .map(i -> i.getRatingsByUser().values())
+        .forEach(ratings::addAll);
 
-    //TODO: Update user
+    return (double) ratings.stream()
+        .mapToInt(Integer::intValue)
+        .sum() / ratings.size();
+
+
+  }
 }
